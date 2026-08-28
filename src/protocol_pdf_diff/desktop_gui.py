2baf4016c64451f94f452924606c638a4542cc12
Riny <rinysemail@gmail.com>
2026-08-28 09:29:12 +0800

    fix: lock swap control during comparison

--- src/protocol_pdf_diff/desktop_gui.py
+++ src/protocol_pdf_diff/desktop_gui.py
@@ -637,6 +637,7 @@ class ProtocolDiffDesktopApp:
             text="交换旧/新",
             command=self._swap_documents,
         )
+        self.input_widgets.append(self.swap_button)
         self.new_document_card = self._create_document_card(
             self.document_cards_host,
             side="new",
